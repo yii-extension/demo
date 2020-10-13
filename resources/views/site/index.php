c7a6a19dd81d5e62036ba3a757f243e76fb8e791
Wilmer Arambula <terabytesoftw@gmail.com>
2020-10-13 12:13:12 -0300

    Add demo FragmentCache widget.

--- resources/views/site/index.php
+++ resources/views/site/index.php
@@ -6,10 +6,20 @@ declare(strict_types=1);
  * @var \App\ApplicationParameters $app
  */
 
+use Yiisoft\Yii\Widgets\FragmentCache;
+
 $this->params['breadcrumbs'] = '/';
 
 $this->setTitle($app->get('app.name'));
+
+$cache = FragmentCache::begin()->id('index');
+
 ?>
 
-<h1 class="title">Hello World</h1>
-<p class="subtitle">My first website with <strong>Yii 3.0</strong>!</p>
+<?php if ($cache->getCachedContent() === null) : ?>
+    <?php $cache->start() ?>
+    <h1 class="title">Hello World</h1>
+    <p class="subtitle">My first website with <strong>Yii 3.0</strong>!</p>
+<?php endif ?>
+
+<?= FragmentCache::end() ?>
